d8a3de34d5bcdbb93d025c0770ef37bd16c8292e
atlansien <atlansien@yahoo.co.jp>
2019-04-06 11:14:30 +0900

    getメソッドの作成

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    message: "メッセージが出力されます"
+  });
+});
+
+app.post("/", (req, res) => {
+  res.render("index", {
+    message: req.body.message
+  });
+});
+
+app.listen(PORT => {
+  console.log(`Exmample app listening on port ${PORT}`);
+});
